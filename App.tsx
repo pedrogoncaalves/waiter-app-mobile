ac28f9cdcba3e83876ad0290ae5bff3843c08eb7
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2023-02-16 15:33:52 -0300

    initializing project and settings

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>MyFirstApp</Text>
+      <Text>Esse é o meu primeiro app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    color: 'red'
+  }
+});
